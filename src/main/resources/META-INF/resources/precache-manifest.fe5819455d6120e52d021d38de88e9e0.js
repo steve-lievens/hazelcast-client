b2ed107e0d071eef6fea6c7d09d05d7cac3b1e0e
Steve Lievens <steve.lievens@be.ibm.com>
2020-07-27 17:31:22 +0200

    React UI (test)
    UI

---
+++ src/main/resources/META-INF/resources/precache-manifest.fe5819455d6120e52d021d38de88e9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7ffa683e2414eb31e395",
+    "url": "/static/js/main.6bc8fd5b.chunk.js"
+  },
+  {
+    "revision": "c2ff783fd43bafbc84ca",
+    "url": "/static/js/2.8378bd06.chunk.js"
+  },
+  {
+    "revision": "7ffa683e2414eb31e395",
+    "url": "/static/css/main.fe4ffbc0.chunk.css"
+  },
+  {
+    "revision": "15da661df3e96d6229af0165e02012e8",
+    "url": "/index.html"
+  }
+];
