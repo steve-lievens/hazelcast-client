40fcd0f77724e6aecf32479d0c8bec51adc46491
Steve Lievens <steve.lievens@be.ibm.com>
2020-07-31 09:51:12 +0200

    ajax call url bug

---
+++ src/main/resources/META-INF/resources/precache-manifest.0175415d211bfa3873da0ebf8e399ebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8bbef4efb45612b656ba",
+    "url": "/static/js/main.3c2487d0.chunk.js"
+  },
+  {
+    "revision": "d2ed1839b023bc7fd7b4",
+    "url": "/static/js/2.1c18e273.chunk.js"
+  },
+  {
+    "revision": "8bbef4efb45612b656ba",
+    "url": "/static/css/main.3c2d745a.chunk.css"
+  },
+  {
+    "revision": "00b096f14c29e9a20603497b6878186e",
+    "url": "/index.html"
+  }
+];
--- src/main/resources/META-INF/resources/service-worker.js
+++ src/main/resources/META-INF/resources/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5236be9ad5212e4470d25384987ad8cb.js"
+  "/precache-manifest.0175415d211bfa3873da0ebf8e399ebf.js"
 );
 
 workbox.clientsClaim();
