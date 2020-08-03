0a6e0bf2486ad88e871fc37b70f3900320daac1b
Steve Lievens <steve.lievens@be.ibm.com>
2020-08-03 11:04:44 +0200

    hazelcast client startup at container start

--- src/main/java/com/ibm/demo/rest/json/HazelcastClientConfig.java
+++ src/main/java/com/ibm/demo/rest/json/HazelcastClientConfig.java
@@ -10,6 +10,10 @@ import com.hazelcast.core.HazelcastInstance;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.event.Observes;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 
 import java.util.logging.Logger;
 
@@ -18,17 +22,27 @@ import java.util.logging.Logger;
 public class HazelcastClientConfig {
 
     private final Logger logger = Logger.getLogger(HazelcastClientConfig.class.getName());
+    private HazelcastInstance hc_instance;
 
     @ConfigProperty(name = "HC_ADDRESS")
     private String hcClusterAddress;
 
-    @Produces
-    HazelcastInstance createInstance() {
+    void onStart(@Observes StartupEvent ev) {               
+        logger.info("The application is starting...");
         logger.info("Hazelcast client connecting to : " + hcClusterAddress);
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.getNetworkConfig().addAddress(hcClusterAddress);
-        HazelcastInstance hc_instance = HazelcastClient.newHazelcastClient(clientConfig);
+        hc_instance = HazelcastClient.newHazelcastClient(clientConfig);
         logger.info("Hazelcast client connected to cluster node " + hc_instance.getName());
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {               
+        logger.info("The application is stopping...");
+    }
+
+    @Produces
+    HazelcastInstance createInstance() {
+    
         return hc_instance;
     }
 }
