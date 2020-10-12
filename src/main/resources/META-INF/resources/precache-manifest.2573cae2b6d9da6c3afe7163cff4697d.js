3ac910937ea52cfa3005464feb59e80e7c15875d
Steve Lievens <steve.lievens@be.ibm.com>
2020-10-12 17:02:10 +0200

    Remove excessive logging

---
+++ src/main/resources/META-INF/resources/precache-manifest.2573cae2b6d9da6c3afe7163cff4697d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b36528f8776453241ef6",
+    "url": "/static/js/main.5217efb1.chunk.js"
+  },
+  {
+    "revision": "3985816ad0bf96da21ef",
+    "url": "/static/js/2.cae753d7.chunk.js"
+  },
+  {
+    "revision": "b36528f8776453241ef6",
+    "url": "/static/css/main.b97fb6af.chunk.css"
+  },
+  {
+    "revision": "daf60d294d31fc1e980e4e5ef51c3dd5",
+    "url": "/index.html"
+  }
+];
--- src/main/resources/META-INF/resources/service-worker.js
+++ src/main/resources/META-INF/resources/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7421d79faaa0b8c5e98f89ba6ab5d3f2.js"
+  "/precache-manifest.2573cae2b6d9da6c3afe7163cff4697d.js"
 );
 
 workbox.clientsClaim();
